89babb0bbd6579bfe15d19255956ad38ae072961
Pavitra Srinivasan <srinivasan.p@husky.neu.edu>
2016-11-16 12:48:14 -0500

    fixed login for invalid user

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -1,12 +1,5 @@
 module.exports = function(app, model) {
 
-    // var users = [
-    //     {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder", email: "alice@gmail.com" },
-    //     {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley", email: "bob@gmail.com" },
-    //     {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia", email: "charly@gmail.com" },
-    //     {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi", email: "jose@gmail.com" }
-    // ];
-
     app.get('/api/user', findUser);
     app.get('/api/user/:uid', findUserById);
     app.post('/api/user',createUser);
@@ -29,14 +22,6 @@ module.exports = function(app, model) {
                 }
             );
 
-        // for(var u in users) {
-        //     if(users[u]._id == uid) {
-        //         users.splice(u,1);
-        //     }
-        // }
-        //
-        // res.sendStatus(200);
-
     }
 
     function updateUser(req, res) {
@@ -56,21 +41,10 @@ module.exports = function(app, model) {
                 }
             );
 
-        // for(var u in users) {
-        //     if(users[u]._id == uid) {
-        //         users[u] = user;
-        //         break;
-        //     }
-        // }
-        //
-        // res.sendStatus(200);
-
     }
 
     function createUser(req, res) {
         var user = req.body;
-        // user._id = (new Date()).getTime();
-        // users.push(user);
 
         model
             .userModel
@@ -124,15 +98,7 @@ module.exports = function(app, model) {
                     res.sendStatus(400).send(error);
                 }
             );
-        
-        // for(var u in users) {
-        //     if(users[u].username === username) {
-        //         res.send(users[u]);
-        //         return;
-        //     }
-        // }
-        //
-        // res.send('0');
+
     }
 
     function findUserById(req, res) {
@@ -159,14 +125,6 @@ module.exports = function(app, model) {
 
             );
 
-        // for(var u in users) {
-        //     if(users[u]._id == userId) {
-        //         res.send(users[u]);
-        //         return;
-        //     }
-        // }
-
-        // res.send('0');
     }
 
     function findUserByCredentials(req, res) {
@@ -177,9 +135,9 @@ module.exports = function(app, model) {
             .userModel
             .findUserByCredentials(username, password)
             .then(
-                function (users) {
-                    if(users) {
-                        res.json(users[0]);
+                function (user) {
+                    if(user) {
+                        res.json(user);
                     }
                     else {
                         res.send('0');
@@ -190,14 +148,5 @@ module.exports = function(app, model) {
                 }
             );
 
-        // for(var u in users) {
-        //     if(users[u].username === username &&
-        //         users[u].password === password) {
-        //         res.send(users[u]);
-        //         return;
-        //     }
-        // }
-        //
-        // res.send('0');
     }
 };
