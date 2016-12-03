0c29e1fca0ed1699894f9c5f1495cf00c87bb81c
Pavitra Srinivasan <srinivasan.p@husky.neu.edu>
2016-12-02 21:14:25 -0500

    added environment variable for MONGODB from config on heroku

--- assignment/model/model.server.js
+++ assignment/model/model.server.js
@@ -3,8 +3,9 @@ module.exports = function () {
     // mongoose.connect('mongodb://127.0.0.1:27017/wam-fall-2016');
 
     var mongoLocalURI = 'mongodb://localhost/wam-fall-2016';
-    mongoose.connect(process.env.MONGODB_URI | mongoLocalURI);
+    mongoose.connect(process.env.MONGODB_URI || mongoLocalURI);
 
+    console.log(process.env.MONGODB_URI);
 
     var userModel = require("./user/user.model.server")();
     var websiteModel = require("./website/website.model.server")();
